433daaa49e237b4b8a12bc53e73ae70db1bc6832
victorjefferson <vrjefferso33@gmail.com>
2023-02-22 08:30:01 -0800

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
